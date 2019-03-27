370fb38ffc4efa300de42b86176f69c41403e634
petetheheat <petetheheat@fb.com>
2019-03-27 13:24:11 -0700

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# petetheheat.github.io
-My personal website
---
+++ precache-manifest.a5eecb9ded6eab48aa2403a89894a07c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "091c0eec83e2e5f3792d",
+    "url": "/react/static/js/main.5f60d924.chunk.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/react/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "1420ae5fc691c07291e705919c4da400",
+    "url": "/react/index.html"
+  }
+];
